53e741a5cdfa238941d04be05cb804d5317d3655
Juan Yepes <juan.yepes@udla.edu.ec>
2026-04-19 23:36:22 -0500

    Calculo de promedios, aprobados y reprobados por asignatura

--- VECTORES-Y-MATRICES.c
+++ VECTORES-Y-MATRICES.c
@@ -28,5 +28,19 @@ int main() {
         }
         printf("Estudiante %d -> Promedio: %.2f\n", i + 1, suma / ASIGNATURAS);
     }
+
+    printf("\nREPORTE POR ASIGNATURA\n");
+    for (int j = 0; j < ASIGNATURAS; j++) {
+        float suma_asig = 0;
+        int aprobados = 0, reprobados = 0; 
+
+        for (int i = 0; i < ESTUDIANTES; i++) {
+            suma_asig += calificaciones[i][j];
+            if (calificaciones[i][j] >= MIN_APROBACION) aprobados++; 
+            else reprobados++;
+        }
+        printf("Asig %d -> Promedio: %.2f | Aprobados: %d | Reprobados: %d\n", 
+                j + 1, suma_asig / ESTUDIANTES, aprobados, reprobados);
+    }
     return 0;
 }

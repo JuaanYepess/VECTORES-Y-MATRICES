506cbb88f19510df7678d7d1440bd2160a724301
Juan Yepes <juan.yepes@udla.edu.ec>
2026-04-19 23:26:56 -0500

    Definicion de constantes y estructura principal

---
+++ VECTORES-Y-MATRICES.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define ESTUDIANTES 5 // [cite: 18]
+#define ASIGNATURAS 3 // [cite: 18]
+#define MIN_APROBACION 6.0 // [cite: 28]
+
+int main() {
+    float calificaciones[ESTUDIANTES][ASIGNATURAS];
+    return 0;
+}

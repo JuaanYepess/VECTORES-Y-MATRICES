e37c7dabb0eec41aad64eb5f341963dff1108374
Juan Yepes <juan.yepes@udla.edu.ec>
2026-04-19 23:29:24 -0500

    Implementacion de lectura de datos con validacion de rango

--- VECTORES-Y-MATRICES.c
+++ VECTORES-Y-MATRICES.c
@@ -6,5 +6,18 @@
 
 int main() {
     float calificaciones[ESTUDIANTES][ASIGNATURAS];
+
+    for (int i = 0; i < ESTUDIANTES; i++) {
+        printf("--- Estudiante %d ---\n", i + 1);
+        for (int j = 0; j < ASIGNATURAS; j++) {
+            do {
+                printf("Ingrese nota Asignatura %d (0-10): ", j + 1);
+                scanf("%f", &calificaciones[i][j]);
+                if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10) {
+                    printf("Error: Nota fuera de rango.\n");
+                }
+            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+        }
+    }
     return 0;
 }
